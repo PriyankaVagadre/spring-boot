d88446a18a1c5f3b658aeaafd66bbd522e843203
priyankavagadre <priyankavagadre@hotmail.com>
2026-06-03 15:53:24 +0530

    messge changed

--- src/main/java/com/demo/spring/StripPaymentService.java
+++ src/main/java/com/demo/spring/StripPaymentService.java
@@ -3,6 +3,6 @@ package com.demo.spring;
 public class StripPaymentService implements PaymentService {
     @Override
     public void processPayment(double amount){
-        System.out.println("amount is" + amount);
+        System.out.println("Strip Service amount is" + amount);
     }
 }
