bd668148fdfa5c324277008541ec22b6cf364177
priyankavagadre <priyankavagadre@hotmail.com>
2026-06-03 17:37:03 +0530

    create a BEAN with the help of ApplicationContext

--- src/main/java/com/demo/spring/Application.java
+++ src/main/java/com/demo/spring/Application.java
@@ -2,15 +2,20 @@ package com.demo.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-//        SpringApplication.run(Application.class, args);
-        var orderService = new OrderService();
-        orderService.setPaymentService(new StripPaymentService());
-        orderService.callPaymentService();
+     ApplicationContext context =  SpringApplication.run(Application.class, args); // creating a context;
+
+     var orderService = context.getBean(OrderService.class); // creating a bean with the help of spring
+         orderService.callPaymentService(); // now we have called directly
+
+//        var orderService = new OrderService();
+//        orderService.setPaymentService(new StripPaymentService()); // use setter for optional dependency
+//        orderService.callPaymentService();
 
     }
 
