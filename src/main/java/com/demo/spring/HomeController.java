e6dfbf42a204cb155beee159cc5a9601616c09b7
priyankavagadre <priyankavagadre@hotmail.com>
2026-06-03 15:15:33 +0530

    call application properties by @Value annotation

--- src/main/java/com/demo/spring/HomeController.java
+++ src/main/java/com/demo/spring/HomeController.java
@@ -1,11 +1,21 @@
 package com.demo.spring;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @Value("${app.pageSize}")
+      private int pageZise;
+
     @RequestMapping("/")
     public String index(){
-        return "index.html";
+        String viewName = getViewNAme();
+        System.out.println(pageZise);
+        return viewName;
+    }
+
+    public String getViewNAme(){
+        return "index";
     }
 }
